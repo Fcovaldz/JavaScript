4023f598d73e99d87c20c93628fd6847a0ed1126
Francisco <franciscobanuelosvaladez@gmail.com>
2021-09-24 10:56:24 -0500

    Operadores ternarios y Arrow functions

---
+++ 13-ternario.js
@@ -0,0 +1,15 @@
+/** 
+ * Operador termario.
+*/
+const edad = 20;
+/*
+if(edad >= 18) {
+    console.log('Eres mayor de edad');
+} else {
+    console.log('Eres menor de edad');
+}*/
+
+/** Estructura de operador termario */
+const mensaje = edad > 18 ? 'Eres mayor de edad' : 'Eres menor de edad'
+
+document.write(`<P>${mensaje}</p>`);
